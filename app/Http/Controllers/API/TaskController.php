c0d3e371786a2712fc3a15521d155abc81a9b098
Praksa 1 <praksa.1@itengine.eu>
2019-11-05 16:56:59 +0100

    Task controller created

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -16,7 +16,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return new TaskResource(Task::all());
+        return new TaskResource(Task::first());
     }
 
     /**
