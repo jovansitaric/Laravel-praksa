cac8730e3c56f3e18befb4a236f49d8cff934e9d
Praksa 1 <praksa.1@itengine.eu>
2019-11-07 11:05:23 +0100

    Update code..

---
+++ app/Http/Controllers/API/NoteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NoteResource;
+use App\Task;
+use App\Note;
+use Auth;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::find();
+        $notes = $tasks->notes;
+
+        return [
+            'tasks' => $tasks,
+            'notes' => $notes
+        ];
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['note_id'] = note()->id;
+        $note = Note::create($data);
+
+        return $note;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Note::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $note = Note::find($id);
+        $note->update($request->all());
+
+        return $note;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Note  $note
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $note = Note::find($id);
+        $note->delete();
+
+        return $note;
+    }
+}
---
+++ app/Note.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $table = "notes";
+
+    protected $fillable = ['title', 'description', 'task_id'];
+
+    public function task() {
+       return $this.hasMany('App\Note');
+    }
+
+}
